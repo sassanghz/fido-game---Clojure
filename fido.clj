d35557b2ca8ba7f009a5bfbf6501a68aea69a975
sassanghz <sassanghazi@gmail.com>
2024-11-23 19:15:14 -0500

    clojure game-fido file selection updated

--- fido.clj
+++ fido.clj
@@ -1,12 +1,7 @@
-;; -------------
-;; This is the menu template for the Fido app. You can use it as a starting
-;; point for the assignment
-;; ------------
-
-(ns a3
+(ns fido
   (:require [clojure.string :as str])
   (:require [clojure.java.io :as io]))
-  ; this is where you would include/require the food module
+  ;; this is where you would include/require the food module if needed
 
 
 ;;-------------------
@@ -21,53 +16,56 @@
   (println "1. Display list of map files")
   (println "2. Display a map for Fido")
   (println "3. Exit")
-  (do 
-    (print "\nEnter an option? ") 
-    (flush) 
+  (do
+    (print "\nEnter an option? ")
+    (flush)
     (read-line)))
 
 
-;; Display all files in the current folder
+;; Display all .txt files
 (defn option1
   []
-  (println "use file-seq to get and print a list of all files in the current folder"))
-    
-    
-    
-;; Get the map specified by the user and start looking for the food
+  (println "\nMap List:")
+  ;; Use file-seq to iterate over files in the current folder
+  (doseq [file (file-seq (io/file "."))]
+    (when (and (.isFile file) (.endsWith (.getName file) ".txt")) 
+      (println (str "* " (.getPath file))))) 
+  (println "\nPress any key to continue...")
+  (flush)
+  (read-line))
+
+
+;; Get the map 
 (defn option2
   []
-  (print "\nPlease enter a file name => ") 
+  (print "\nPlease enter a file name => ")
   (flush)
-  (let [file_name (read-line)]
-     (println "now read" file_name "with slurp and try to find Fido's food")))
+  (let [file_name (read-line)] 
+    (if (.exists (io/file file_name))
+      (println "now read" file_name "with slurp and try to find Fido's food")
+      (println "Error: File does not exist"))))
 
 
 
-; If the menu selection is valid, call the relevant function to 
-; process the selection
+;; If the menu selection is valid, call the function
 (defn processOption
-  [option] 
-  (if( = option "1")
-     (option1)
-     (if( = option "2")
-        (option2)   
-        (println "Invalid menu option"))))
+  [option]
+  (cond
+    (= option "1") (option1)
+    (= option "2") (option2)
+    :else (println "Invalid menu option")))
 
 
-; Display the menu and get a menu item selection. Process the
-; selection and then loop again to get the next menu selection
+;; Display the menu and get a menu item selection. Process the
+;; selection and then loop again to get the next menu selection
 (defn menu
-  [] ; parm(s) can be provided here, if needed
+  [] 
   (let [option (str/trim (showMenu))]
     (if (= option "3")
       (println "\nGood Bye\n")
-      (do 
-         (processOption option)
-         (recur )))))   
+      (do
+        (processOption option)
+        (recur)))))
 
 
-
-; ------------------------------
-; Run the program.
 (menu)
