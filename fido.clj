a9c4336e51991172e6a73da0483fa0b607237f8a
sassanghz <sassanghazi@gmail.com>
2024-11-24 20:34:59 -0500

    adjustment for files

--- fido.clj
+++ fido.clj
@@ -1,18 +1,16 @@
 (ns fido
-  (:require [clojure.string :as str])
-  (:require [clojure.java.io :as io]))
-  ;; this is where you would include/require the food module if needed
-
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]
+            [food])) ;; Import food namespace
 
 ;;-------------------
 ;; THE MENU FUNCTIONS
 ;; ------------------
 
 ;; Display the menu and ask the user for the option
-(defn showMenu
-  []
+(defn show-menu []
   (println "\n\n*** Let's Feed Fido ***")
-  (println     "-----------------------\n")
+  (println "-----------------------\n")
   (println "1. Display list of map files")
   (println "2. Display a map for Fido")
   (println "3. Exit")
@@ -21,51 +19,41 @@
     (flush)
     (read-line)))
 
+;; Option 1: Display all .txt files
+(defn option1 []
+  (food/list-files)) ; Call the list-files function from food.clj
 
-;; Display all .txt files
-(defn option1
-  []
-  (println "\nMap List:")
-  ;; Use file-seq to iterate over files in the current folder
-  (doseq [file (file-seq (io/file "."))]
-    (when (and (.isFile file) (.endsWith (.getName file) ".txt")) 
-      (println (str "* " (.getPath file))))) 
-  (println "\nPress any key to continue...")
-  (flush)
-  (read-line))
-
-
-;; Get the map 
-(defn option2
-  []
+;; Option 2: Read and display the map
+(defn option2 []
   (print "\nPlease enter a file name => ")
   (flush)
-  (let [file_name (read-line)] 
-    (if (.exists (io/file file_name))
-      (println "now read" file_name "with slurp and try to find Fido's food")
-      (println "Error: File does not exist"))))
+  (let [file-name (read-line)
+        map-data (food/read-map file-name)] ; Call read-map from food.clj
+    (when map-data
+      (println "\nThis is Fido's challenge:")
+      (doseq [line map-data]
+        (println (apply str line)))
+      (let [updated-map (food/find-food map-data 0 0)]
+        (println "\nFinal Map:")
+        (doseq [line updated-map]
+          (println (apply str line)))))))
 
-
-
-;; If the menu selection is valid, call the function
-(defn processOption
-  [option]
+;; If the menu selection is valid, call the relevant function
+(defn process-option [option]
   (cond
     (= option "1") (option1)
     (= option "2") (option2)
     :else (println "Invalid menu option")))
 
-
 ;; Display the menu and get a menu item selection. Process the
 ;; selection and then loop again to get the next menu selection
-(defn menu
-  [] 
-  (let [option (str/trim (showMenu))]
+(defn menu []
+  (let [option (str/trim (show-menu))]
     (if (= option "3")
       (println "\nGood Bye\n")
       (do
-        (processOption option)
+        (process-option option)
         (recur)))))
 
-
+;; Run the program
 (menu)
--- food.clj
+++ food.clj
@@ -1,12 +1,49 @@
 (ns food
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
+;; List all .txt files in the current directory
 (defn list-files []
   (println "Map List:")
-  ;; traverse the dir and .txt files
   (doseq [file (file-seq (io/file "."))]
     (when (and (.isFile file) (.endsWith (.getName file) ".txt"))
       (println (str "* " (.getPath file)))))
   (println "\nPress any key to continue")
-  (flush) 
-  (read-line)) ; Wait for the user to press the key
+  (flush)
+  (read-line)) ; Wait for the user to press a key
+
+;; Read the map and convert it into a 2D data structure
+(defn read-map [file-name]
+  (let [file (io/file file-name)]
+    (if (.exists file)
+      (let [file-content (slurp file)
+            map-data (vec (map vec (str/split-lines file-content)))]
+        (println "\nMap File Content:")
+        (println file-content)
+        map-data)
+      (do
+        (println "Oops: specified file does not exist")
+        nil))))
+
+
+(defn find-food [map-data row col]
+  (let [rows (count map-data)
+        cols (count (first map-data))]
+    (cond
+      (or (< row 0) (>= row rows) (< col 0) (>= col cols)) false
+      (= (get-in map-data [row col]) \@) (do
+                                           (assoc-in map-data [row col] \+)
+                                           (println "\nWoo Hoo - Fido found her food!")
+                                           true)
+      (or (= (get-in map-data [row col]) \#)
+          (= (get-in map-data [row col]) \+)
+          (= (get-in map-data [row col]) \!)) false
+      :else ((let [updated-map (assoc-in map-data [row col] \+)];; creates a new map but doe not modify the original
+                (if (or
+                     (find-food updated-map (dec row) col) ;up
+                     (find-food updated-map (inc row) col) ;down
+                     (find-food updated-map row (dec col)) ;left
+                     (find-food updated-map row (inc col))) ;right   
+                     updated-map
+                  (assoc-in updated-map [row col] \!))))))
+  )
